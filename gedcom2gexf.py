07be3ffc0c70fa52e1f8149b6d2d11e5e3ce2b50
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2016-10-23 21:38:04 +0200

    Added first script to parse gedcom file

---
+++ gedcom2gexf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Works with Python2 and Python3
+
+import gedcom
+import networkx as nx
+import os
+
+def gedcom2gephi(gedcomFilename = 'gedcom.ged', gephiFilename = None):
+    getName = lambda n: n.name[0]+' '+n.name[1]
+    getId = lambda n: n.id[1:-1]
+    getFamilyName = lambda n: n.name[1]
+
+    g = gedcom.parse(gedcomFilename)
+    dg = nx.DiGraph()
+    for p in g.individuals:
+        if p.id not in dg:
+            dg.add_node(getId(p), {'label':getName(p), 'name':getName(p), 'familyName':getFamilyName(p)})
+    for p in g.individuals:
+        if p.father:
+            dg.add_edge(getId(p.father), getId(p))
+        if p.mother:
+            dg.add_edge(getId(p.mother), getId(p))
+    if gephiFilename is None:
+        gephiFilename = os.path.splitext(gedcomFilename)[0]+'.gexf'
+    nx.write_gexf(dg, gephiFilename)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description = 'This script converts a gedcom file to a gexf file')
+    parser.add_argument('-g','--gedcom', type = str, default = 'my_gedcom_file.ged',
+                       help = 'gedcom filename')
+    parser.add_argument('-o','--outputGexf', type = str, default = None,
+                       help='')
+    args = parser.parse_args()
+    gedcom2gephi(gedcomFilename = args.gedcom, gephiFilename =args.outputGephi)
