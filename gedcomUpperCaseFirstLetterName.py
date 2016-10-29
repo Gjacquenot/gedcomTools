02702c753c0cb07789521642bf2b18dd9969fb40
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2016-10-29 17:02:22 +0200

    Updated documentation

--- gedcomUpperCaseFirstLetterName.py
+++ gedcomUpperCaseFirstLetterName.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# This script geopy to find the latitude and longitude
-# of locations.
+# This script reads a gedcom file and changes the case
+# of family names: it uppers case the first letter of the family name.
+# The result is exported in a new file.
 #
-# Since we can not perform too much requests, this script
-# stores the result of the requests in a text file.
+# This script does not require any additional library to work.
 
 def upperCaseFirstLetterName(gedcomFilename, outputFilename = None):
+    """
+    gedcomFilename : str of the gedcom file to import
+    outputFilename : str of the output gedcom to generate. If None, 
+                     the function appends '_WithUpperCase.ged'
+                     to the gedcomFilename
+    One uses regular expression to match all expressions between two slash    
+    """
     import os
     import re
     def repl_func(m):
         """process regular expression match groups for word upper-casing problem"""
         name = m.group(2)
         upperCaseName = ' '.join([n[0].upper()+n[1:].lower() for n in name.split()])
-        return m.group(1)+'/'+upperCaseName+'/'+m.group(3)
-    gg = re.compile('(.*)\/(.*)\/(.*)',flags=re.UNICODE)
+        return m.group(1) + '/' + upperCaseName + '/' + m.group(3)
+    gg = re.compile('(.*)\/(.*)\/(.*)', flags=re.UNICODE)
     lines = [c.strip() for c in open(gedcomFilename, 'r', encoding='utf-8').readlines()]
     newLines = [gg.sub(repl_func, line) for line in lines]
     if outputFilename is None:
-        outputFilename = os.path.splitext(gedcomFilename)[0]+'_WithUpperCase.ged'
+        outputFilename = os.path.splitext(gedcomFilename)[0] + '_WithUpperCase.ged'
     open(outputFilename, 'w').writelines('\n'.join(newLines))
 
 if __name__ == '__main__':
